3cbfc9db8963a18ffd75b33a8d5aa08394b08c6f
wangzhy <wangzhy@wangzhydeMacBook-Pro.local>
2025-11-14 23:16:00 +0800

    update

---
+++ proxy/gen.sh
@@ -0,0 +1,3 @@
+for i in {1..13}; do
+  echo "vless://2892e9f9-a530-44c9-9bd3-6ab235928b4e@yg${i}.ygkkk.dpdns.org:8880?encryption=none&security=none&sni=worker-jp.crazycry2010.workers.dev&fp=chrome&type=ws&host=worker-jp.crazycry2010.workers.dev&path=%2F#jp-yg${i}"
+done
